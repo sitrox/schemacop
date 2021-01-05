8e2d42d76a059fc35b1ee87a8ab7a1e02f756d1c
Adrian Hirt <aedu.hirt@gmail.com>
2021-01-05 15:04:11 +0100

    Add test for generic keywords for isNot

--- test/unit/schemacop/v3/is_not_node_test.rb
+++ test/unit/schemacop/v3/is_not_node_test.rb
@@ -36,6 +36,64 @@ module Schemacop
           error '/', 'Must not match schema: {"type"=>"integer", "minimum"=>5}.'
         end
       end
+
+      def test_enum_schema
+        schema :is_not do
+          str enum: ['bar', 'qux', 123, :faz]
+        end
+
+        assert_json({
+                      not: {
+                        type: :string,
+                        enum: ['bar', 'qux', 123, :faz]
+                      }
+                    })
+        assert_validation(nil)
+        assert_validation({ foo: 'bar' })
+        assert_validation('baz')
+        assert_validation(123)
+        assert_validation(:faz)
+
+        # Needs to fail the validation, as the value is included in the enum
+        # and a string (which the not turns into "failing the validation")
+        assert_validation('bar') do
+          error '/', 'Must not match schema: {"type"=>"string", "enum"=>["bar", "qux", 123, "faz"]}.'
+        end
+
+        assert_validation('qux') do
+          error '/', 'Must not match schema: {"type"=>"string", "enum"=>["bar", "qux", 123, "faz"]}.'
+        end
+      end
+
+      def test_with_generic_keywords
+        schema :is_not do
+          str enum: ['bar', 'qux', 123, :faz], title: 'Short title', description: 'Longer description', examples: ['foo']
+        end
+
+        assert_json({
+                      not: {
+                        type:        :string,
+                        enum:        ['bar', 'qux', 123, :faz],
+                        title:       'Short title',
+                        description: 'Longer description',
+                        examples:    ['foo']
+                      }
+                    })
+
+        # rubocop:disable Layout/LineLength
+
+        # Needs to fail the validation, as the value is included in the enum
+        # and a string (which the not turns into "failing the validation")
+        assert_validation('bar') do
+          error '/', 'Must not match schema: {"type"=>"string", "title"=>"Short title", "examples"=>["foo"], "description"=>"Longer description", "enum"=>["bar", "qux", 123, "faz"]}.'
+        end
+
+        assert_validation('qux') do
+          error '/', 'Must not match schema: {"type"=>"string", "title"=>"Short title", "examples"=>["foo"], "description"=>"Longer description", "enum"=>["bar", "qux", 123, "faz"]}.'
+        end
+
+        # rubocop:enable Layout/LineLength
+      end
     end
   end
 end
