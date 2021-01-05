865f5e8e1d1ad0960018c49b5fbda780eefe6098
Adrian Hirt <aedu.hirt@gmail.com>
2021-01-05 15:05:16 +0100

    Add test for generic keywords for symbol

--- test/unit/schemacop/v3/symbol_node_test.rb
+++ test/unit/schemacop/v3/symbol_node_test.rb
@@ -39,6 +39,34 @@ module Schemacop
         assert_validation %i[foo bar baz]
         assert_json(type: :array, items: {}, additionalItems: false)
       end
+
+      def test_enum_schema
+        schema :symbol, enum: [1, 2, 'foo', :bar, { qux: 42 }]
+
+        # For symbol nodes, json representation is an empty hash, as we can't
+        # repsresent symbols in json
+        assert_json({})
+
+        assert_validation(nil)
+        assert_validation(:bar)
+
+        # Even we put those types in the enum, they need to fail the validations,
+        # as they are not symbols
+        assert_validation('foo') do
+          error '/', 'Invalid type, expected "Symbol".'
+        end
+        assert_validation(1) do
+          error '/', 'Invalid type, expected "Symbol".'
+        end
+        assert_validation({ qux: 42 }) do
+          error '/', 'Invalid type, expected "Symbol".'
+        end
+
+        # These need to fail validation, as they are not in the enum list
+        assert_validation(:foo) do
+          error '/', 'Value not included in enum [1, 2, "foo", :bar, {:qux=>42}].'
+        end
+      end
     end
   end
 end
