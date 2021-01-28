ab2ae96c6f3918451108afd2e35f941e69c97bcb
Remo Fritzsche <remo.fritzsche@sitrox.com>
2021-01-28 14:22:24 +0100

    Make 'sanitize_exp` a class method so it can be accessed from the outside

--- lib/schemacop/v3/hash_node.rb
+++ lib/schemacop/v3/hash_node.rb
@@ -21,6 +21,14 @@ module Schemacop
         super + NodeRegistry.dsl_methods(true) + %i[dsl_dep dsl_add]
       end
 
+      def self.sanitize_exp(exp)
+        exp = exp.to_s
+        if exp.start_with?('(?-mix:')
+          exp = exp.to_s.gsub(/^\(\?-mix:/, '').gsub(/\)$/, '')
+        end
+        return exp
+      end
+
       def add_child(node)
         unless node.name
           fail Exceptions::InvalidSchemaError, 'Child nodes must have a name.'
@@ -56,7 +64,7 @@ module Schemacop
 
         json = {}
         json[:properties] = Hash[properties.values.map { |p| [p.name, p.as_json] }] if properties.any?
-        json[:patternProperties] = Hash[pattern_properties.values.map { |p| [sanitize_exp(p.name), p.as_json] }] if pattern_properties.any?
+        json[:patternProperties] = Hash[pattern_properties.values.map { |p| [self.class.sanitize_exp(p.name), p.as_json] }] if pattern_properties.any?
 
         # In schemacop, by default, additional properties are not allowed,
         # the users explicitly need to enable additional properties
@@ -75,14 +83,6 @@ module Schemacop
         end
 
         return process_json(ATTRIBUTES, json)
-      end
-
-      def sanitize_exp(exp)
-        exp = exp.to_s
-        if exp.start_with?('(?-mix:')
-          exp = exp.to_s.gsub(/^\(\?-mix:/, '').gsub(/\)$/, '')
-        end
-        return exp
       end
 
       def allowed_types
